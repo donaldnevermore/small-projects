c3fdd571b882d7f90f95f8e78deb676401922116
Donald33 Wang <dongcheng6@qq.com>
2021-03-10 23:21:39 +0800

    Add pagemaker

---
+++ pagemaker/pagemaker.py
@@ -0,0 +1,37 @@
+from xml.sax.handler import ContentHandler
+from xml.sax import parse
+from io import TextIOWrapper
+
+
+class PageMaker(ContentHandler):
+    passthrough: bool = False
+    out: TextIOWrapper
+
+    def startElement(self, name: str, attrs) -> None:
+        if name == "page":
+            self.passthrough = True
+            self.out = open(attrs["name"] + ".html", "w")
+            self.out.write("<html><head>\n")
+            self.out.write(f"<title>{attrs['title']}</title>\n")
+            self.out.write("</head><body>\n")
+        elif self.passthrough:
+            self.out.write(f"<{name}")
+            for key, val in attrs.items():
+                self.out.write(f' {key}="{val}"')
+            self.out.write(">")
+
+    def endElement(self, name: str) -> None:
+        if name == "page":
+            self.passthrough = False
+            self.out.write("\n</body></html>\n")
+            self.out.close()
+        elif self.passthrough:
+            self.out.write(f"</{name}>")
+
+    def characters(self, content: str) -> None:
+        if self.passthrough:
+            self.out.write(content)
+
+
+if __name__ == "__main__":
+    parse("./website.xml", PageMaker())
---
+++ pagemaker/parse_xml.py
@@ -0,0 +1,37 @@
+from typing import Any, List
+from xml.sax.handler import ContentHandler
+from xml.sax import parse
+
+
+class HeadlineHandler(ContentHandler):
+    in_headline: bool = False
+    headlines: List[str]
+    data: List[str]
+
+    def __init__(self, headlines) -> None:
+        super().__init__()
+        self.headlines = headlines
+        self.data = []
+
+    def startElement(self, name: str, attrs) -> None:
+        if name == "h1":
+            self.in_headline = True
+
+    def endElement(self, name: str) -> None:
+        if name == "h1":
+            text = "".join(self.data)
+            self.data = []
+            self.headlines.append(text)
+            self.in_headline = False
+
+    def characters(self, content: str) -> None:
+        if self.in_headline:
+            self.data.append(content)
+
+
+if __name__ == "__main__":
+    headlines = []
+    parse("./website.xml", HeadlineHandler(headlines))
+    print("The following <h1> elements were found:")
+    for h in headlines:
+        print(h)
